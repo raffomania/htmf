7cad5f06117ee31b506716312de28d3ddaa5c28b
Rafael Epplée <hello@rafa.ee>
2024-08-17 17:11:23 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+use std::borrow::Cow;
+
+fn a<'a, const A: usize, S, C>(attrs: [(&'static str, S); A], children: C) -> Element<'a>
+where
+    S: Into<Cow<'a, str>>,
+    C: Into<Vec<Element<'a>>>,
+{
+    Element::Tag {
+        tag: "a".to_string(),
+        attrs: attrs.into_iter().map(|(k, v)| (k, v.into())).collect(),
+        children: children.into(),
+    }
+}
+
+fn href(target: &str) -> (&'static str, &str) {
+    ("href", target)
+}
+
+type Attr<'a> = (&'static str, Cow<'a, str>);
+
+#[derive(PartialEq, Eq, Debug)]
+enum Element<'a> {
+    Tag {
+        tag: String,
+        attrs: Vec<Attr<'a>>,
+        children: Vec<Element<'a>>,
+    },
+    Text(Cow<'a, str>),
+}
+
+impl<'a> Element<'a> {
+    fn to_html(&'a self) -> String {
+        match self {
+            Element::Tag {
+                tag,
+                attrs,
+                children,
+            } => {
+                let children_html: String = children.iter().map(|c| c.to_html()).collect();
+                let attrs_html: String =
+                    attrs.iter().map(|(k, v)| format!(r#"{k}="{v}""#)).collect();
+                format!("<{tag} {attrs_html}>{children_html}</{tag}>")
+            }
+            Element::Text(t) => t.to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let elem = a(
+            [href("https://www.rafa.ee")],
+            [Element::Text("My Site".into())],
+        );
+        let expected_html = r#"<a href="https://www.rafa.ee">My Site</a>"#;
+        assert_eq!(elem.to_html(), expected_html);
+    }
+}
